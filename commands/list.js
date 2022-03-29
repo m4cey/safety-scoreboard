dedc6f6ee0485743a11099ee95ebfb7b5a51f00d
m4cey <h4mmer7ime@gmail.com>
2022-03-29 11:41:03 +0000

    testing

--- commands/list.js
+++ commands/list.js
@@ -1,48 +1,53 @@
 const StormDB = require("stormdb");
-const { SlashCommandBuilder } = require('@discordjs/builders');
-const { MessageEmbed } = require('discord.js');
-const moment = require('moment');
-
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { MessageEmbed } = require("discord.js");
+const moment = require("moment");
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('list')
-		.setDescription('list all keywords'),
-	async execute(interaction) {
-		const engine = new StormDB.localFileEngine("./db.stormdb");
-		const db = new StormDB(engine);
-		const data = db.get('keywords').value();
-		if (!data) {
-			await interaction.reply('nada');
-			return;
-		}
+  data: new SlashCommandBuilder()
+    .setName("list")
+    .setDescription("list all keywords"),
+  async execute(interaction) {
+    await interaction.deferReply();
+    const engine = new StormDB.localFileEngine("./db.stormdb");
+    const db = new StormDB(engine);
+    const data = db.get("keywords").value();
+    if (!data) {
+      await interaction.editReply("nada");
+      return;
+    }
 
-		const fieldNames = [];
-		for (obj of data) {
-			const fieldName = {};
-			const keyword = Object.keys(obj)[0];
-			const time = Object.values(obj)[0];
-			const timeScore = moment().diff(moment(time));
-			const timeMode = db.get('timemode').value();
-			const occurences = obj['count'];
-			if (occurences) {
-				fieldName.name = `${keyword.toUpperCase()} | ${occurences || 0} oopsies`;
-				fieldName.value = `Last accident: ${Math.round(moment.duration(timeScore).as(timeMode))} ${timeMode} ago`;
-			} else {
-				fieldName.name = keyword.toUpperCase();
-				fieldName.value = 'No recorded accidents :)';
-			}
-			fieldName.inline = true;
-			fieldNames.push(fieldName);
-		}
-		const embed = new MessageEmbed().setColor('0x00FFFF')
-			.setTitle('Currentely watched for accidents:')
-			.setAuthor({ name: 'Safety First :)' })
-			.setDescription('you are being watched o_o')
-			.addFields(fieldNames)
-			.setFooter({ text: 'o__o' });
-		if (data.length === 0)
-			embed.setDescription('there\'s nothing here... add smth using /track')
-		await interaction.reply({ embeds: [embed]});
-	},
+    const fieldNames = [];
+    for (obj of data) {
+      const fieldName = {};
+      const keyword = Object.keys(obj)[0];
+      const time = Object.values(obj)[0];
+      const timeScore = moment().diff(moment(time));
+      const timeMode = db.get("timemode").value();
+      const occurences = obj["count"];
+      if (occurences) {
+        fieldName.name = `${keyword.toUpperCase()} | ${
+          occurences || 0
+        } oopsies`;
+        fieldName.value = `Last accident: ${Math.round(
+          moment.duration(timeScore).as(timeMode)
+        )} ${timeMode} ago`;
+      } else {
+        fieldName.name = keyword.toUpperCase();
+        fieldName.value = "No recorded accidents :)";
+      }
+      fieldName.inline = true;
+      fieldNames.push(fieldName);
+    }
+    const embed = new MessageEmbed()
+      .setColor("0x00FFFF")
+      .setTitle("Currentely watched for accidents:")
+      .setAuthor({ name: "Safety First :)" })
+      .setDescription("you are being watched o_o")
+      .addFields(fieldNames)
+      .setFooter({ text: "o__o" });
+    if (data.length === 0)
+      embed.setDescription("there's nothing here... add smth using /track");
+    await interaction.editReply({ embeds: [embed] });
+  },
 };
--- index.js
+++ index.js
@@ -1,81 +1,84 @@
 // Require the necessary discord.js classes
-const fs = require('node:fs');
-const { Client, Collection, Intents, MessageEmbed } = require('discord.js');
-const { token } = require('./config.json');
-const StormDB = require('stormdb');
-const moment = require('moment');
-const { setKeyword, setScore, incrementCount } = require('./helper.js');
+const fs = require("node:fs");
+const { Client, Collection, Intents, MessageEmbed } = require("discord.js");
+const { token } = require("./config.json");
+const StormDB = require("stormdb");
+const moment = require("moment");
+const { setKeyword, setScore, incrementCount } = require("./helper.js");
 
 // Create a new client instance
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+});
 
 // Dynamic command handling
 client.commands = new Collection();
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commandFiles = fs
+  .readdirSync("./commands")
+  .filter((file) => file.endsWith(".js"));
 
 for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	// Set a new item in the Collection
-	// With the key as the command name and the value as the exported module
-	client.commands.set(command.data.name, command);
+  const command = require(`./commands/${file}`);
+  // Set a new item in the Collection
+  // With the key as the command name and the value as the exported module
+  client.commands.set(command.data.name, command);
 }
 
 // When the client is ready, run this code (only once)
 const engine = new StormDB.localFileEngine("./db.stormdb");
 const db = new StormDB(engine);
-db.default({ timemode: 'minutes', keywords: [] }).save();
+db.default({ timemode: "minutes", keywords: [] }).save();
 moment().format();
 
-client.once('ready', () => {
-	console.log('Ready!');
+client.once("ready", () => {
+  console.log("Ready!");
 });
 
 // Read and reply to registered commands
-client.on('interactionCreate', async interaction => {
-	if (!interaction.isCommand())	return;
-	const command = client.commands.get(interaction.commandName);
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isCommand()) return;
+  const command = client.commands.get(interaction.commandName);
 
-	if (!command) return;
+  if (!command) return;
 
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error)
-		await interaction.reply({ content: 'no work, sorry :(', ephemeral: true });
-	}
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    await interaction.reply({ content: "no work, sorry :(", ephemeral: true });
+  }
 });
 
 // Main functionality
-client.on('messageCreate', async message => {
-	const engine = new StormDB.localFileEngine("./db.stormdb");
-	const db = new StormDB(engine);
+client.on("messageCreate", async (message) => {
+  const engine = new StormDB.localFileEngine("./db.stormdb");
+  const db = new StormDB(engine);
 
-	if (message.author.bot) return;
-	const data = db.get('keywords').value();
-	if (!data)
-		return;
+  if (message.author.bot) return;
+  const data = db.get("keywords").value();
+  if (!data) return;
 
-	for (obj of data) {
-		const keyword = Object.keys(obj)[0];
-		const oldTimestamp = Object.values(obj)[0];
-		const regexp = new RegExp(keyword, 'gi');
-		const match = regexp.test(message.content);
-		if (match) {
-			setKeyword(keyword, message.createdTimestamp);
+  for (obj of data) {
+    const keyword = Object.keys(obj)[0];
+    const oldTimestamp = Object.values(obj)[0];
+    const regexp = new RegExp(keyword, "gi");
+    const match = regexp.test(message.content);
+    if (match) {
+      setKeyword(keyword, message.createdTimestamp);
 
-			const timeScore = moment().diff(moment(oldTimestamp));
-			// const timeMode = db.get('timemode').value();
-			setScore(keyword, message.member.user.id, timeScore);
-			incrementCount(keyword);
+      const timeScore = moment().diff(moment(oldTimestamp));
+      // const timeMode = db.get('timemode').value();
+      setScore(keyword, message.member.user.id, timeScore);
+      incrementCount(keyword);
 
-			// const embed = new MessageEmbed().setColor('0x00FFFF')
-			// 	.setTitle(`~~${Math.round(moment.duration(timeScore).as(timeMode))}~~ **0 ${timeMode.toUpperCase()}**`)
-			// 	.setAuthor({ name: 'This place has worked:'})
-			// 	.setDescription(`without a **${keyword}** related accident!`)
-			// 	.setFooter({text: `${obj['count'] + 1 || 1} oopsies`});
-			// message.reply({ embeds: [embed] });
-		}
-	}
+      // const embed = new MessageEmbed().setColor('#00FFFF')
+      // 	.setTitle(`~~${Math.round(moment.duration(timeScore).as(timeMode))}~~ **0 ${timeMode.toUpperCase()}**`)
+      // 	.setAuthor({ name: 'This place has worked:'})
+      // 	.setDescription(`without a **${keyword}** related accident!`)
+      // 	.setFooter({text: `${obj['count'] + 1 || 1} oopsies`});
+      // message.reply({ embeds: [embed] });
+    }
+  }
 });
 
 // Login to Discord with your client's token
